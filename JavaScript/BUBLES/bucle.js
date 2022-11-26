7817815bc20f0c872e2563bf681eb1fcf0592089
Camilo Montaguth <Montaguthc@hotmail.com>
2022-11-25 20:13:12 -0500

    BUCLE

---
+++ JavaScript/BUBLES/bucle.js
@@ -0,0 +1,37 @@
+// let cantidad = prompt ("Cuantos alumnos son?");
+// let alumnosTotales = [];
+
+// for (i = 0; i< cantidad; i++) {
+//     alumnosTotales [i] = [prompt("Nombre del alumno" + (i))];
+
+// }
+// const tomarAsistencia = (nombre, p) => {
+//     let presencia = prompt(nombre);
+//     if (presencia == "p".toLowerCase){
+//     alumnosTotales[i][1]++;
+//     }
+// }
+
+// for (i = 0; i < 30; i++) {
+//     for (alumno in alumnosTotales) {
+//         tomarAsistencia(alumnosTotales[alumno][0],alumno);
+//     }
+// }
+
+// for (alumno in alumnosTotales) {
+//     let resultado = `${alumnosTotales[alumno][0]}:<br>
+//     ______Presentes: ${alumnosTotales[alumno][1]} <br>
+//     ______Ausencias: ${30 - alumnosTotales[alumno[1]]}
+//     `;
+//     if (30 - alumnosTotales[alumno][1] > 18 ) {
+//         resultado += "<b style ='color:red'>REPROBADO POR INASISTENCIA </b><br><br>";
+//     } else {
+//         resultado += "<br><br>"
+//     } 
+// }
+//  let nombre = "camilo" ;
+
+// console.log (nombre.toUpperCase ());
+
+let nombre = prompt ("ingresr su nombre ");
+console.log (nombre.toUpperCase ());
